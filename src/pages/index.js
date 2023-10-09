989139ff6da19ffecf025346f63ce3d045152b9d
hotehama2022 <hotehama-r@cin-group.com>
2023-10-09 12:08:10 +0900

    Stateのオブジェクト化

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,22 @@ export default function Home() {
    * 直2 = ステートを更新するための関数(set〇〇と表すことが多い)
    */
   const [ count, setCount ] = useState(0)
+
+  // オブジェクト型も保持することが可能
+  const [ person, setPerson ] = useState({
+    name: '田中',
+    age: 20,
+    isMale: true
+  })
+
+  // 性別を変える
+  const updateMale = () => {
+    setPerson((prevPerson) => ({
+      ...prevPerson,
+      isMale: !prevPerson.isMale
+    }))
+  }
+
   return (
     <>
       <Head>
@@ -26,6 +42,20 @@ export default function Home() {
           onClick={() => setCount((count) => count + 1)}
         >
           Click
+        </button>
+      </div>
+      <div>
+        <ul>
+          <li>私の名前は{person.name}です</li>
+          <li>年齢は{person.age}歳です</li>
+          <li>性別は{person.isMale ? '男' : '女'}です</li>
+        </ul>
+      </div>
+      <div>
+        <button
+          onClick={updateMale}
+        >
+          性別を変える
         </button>
       </div>
     </>
